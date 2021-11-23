2c972cc2825f0cc9236f3d8c7d81af957d3c8e63
thienjs <thienjsx@gmail.com>
2021-11-23 15:07:11 -0500

    seo and manifest change

--- pages/index.tsx
+++ pages/index.tsx
@@ -58,8 +58,8 @@ export default function Home({
         ))}
       </Marquee>
       <Hero
-        headline=" Dessert dragée halvah croissant."
-        description="Cupcake ipsum dolor sit amet lemon drops pastry cotton candy. Sweet carrot cake macaroon bonbon croissant fruitcake jujubes macaroon oat cake. Soufflé bonbon caramels jelly beans. Tiramisu sweet roll cheesecake pie carrot cake. "
+        headline="T & T Nail Supply"
+        description="Best Quality, Lowest Prices "
       />
       <Grid layout="B" variant="filled">
         {products.slice(0, 3).map((product: any, i: number) => (
